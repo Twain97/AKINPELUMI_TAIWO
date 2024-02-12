0f922f8a59aed034f8f20da4059f85bb7eb3c242
Akinpelumi Taiwo <sammytwain97@gmail.com>
2024-02-12 18:00:44 +0100

    deleted dist removed '/' at the emd of base in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/AKINPELUMI_TAIWO/',
+  base:"/AKINPELUMI_TAIWO",
   plugins: [
     vue(),
   ],
